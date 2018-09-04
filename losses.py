e7c1bfa2f341b377c810e206d2b3a4e72ca00f1f
Arjun Desai <arjunaashna@gmail.com>
2018-09-04 13:36:23 -0700

    add train with weighted cross entropy

--- losses.py
+++ losses.py
@@ -73,6 +73,8 @@ def weighted_categorical_crossentropy(weights):
     weights = K.variable(weights)
 
     def loss(y_true, y_pred):
+        print(K.shape(y_true))
+        print(K.shape(y_pred))
         # scale predictions so that the class probas of each sample sum to 1
         y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
         # clip to prevent NaN's and Inf's
