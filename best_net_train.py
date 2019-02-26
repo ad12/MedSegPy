a51d673ee13dfc431a43063f8224ca3f9dd72c77
Arjun Desai <arjunaashna@gmail.com>
2019-02-25 17:11:39 -0800

    update train

--- best_net_train.py
+++ best_net_train.py
@@ -5,7 +5,7 @@ import config as MCONFIG
 import glob_constants
 import oai_train
 from config import DeeplabV3Config
-from losses import BINARY_CROSS_ENTROPY_SIG_LOSS, WEIGHTED_CROSS_ENTROPY_LOSS
+from losses import BINARY_CROSS_ENTROPY_SIG_LOSS, WEIGHTED_CROSS_ENTROPY_LOSS, FOCAL_LOSS
 import numpy as np
 
 CLASS_WEIGHTS = np.asarray([100, 1])
@@ -32,6 +32,12 @@ if __name__ == '__main__':
     #oai_train.fine_tune('/bmrNAS/people/arjun/msk_seg_networks/best_network/deeplabv3_2d/2018-11-27-00-40-24/', DeeplabV3Config(), vals_dict={'INITIAL_LEARNING_RATE':8e-6})
     #oai_train.train(DeeplabV3Config(), vals_dict={'AUGMENT_DATA': False, 'N_EPOCHS': 100, 'LOSS': BINARY_CROSS_ENTROPY_SIG_LOSS})
     # oai_train.train(DeeplabV3Config(), vals_dict={'AUGMENT_DATA': True, 'N_EPOCHS': 50, 'LOSS': BINARY_CROSS_ENTROPY_SIG_LOSS})
-    oai_train.train(DeeplabV3Config(), {'N_EPOCHS': 100, 'TRAIN_BATCH_SIZE': 12, 'USE_STEP_DECAY': False,
-                                        'AUGMENT_DATA': False, 'LOSS': WEIGHTED_CROSS_ENTROPY_LOSS,
-                                        'INCLUDE_BACKGROUND': True}, class_weights=CLASS_WEIGHTS)
+    # oai_train.train(DeeplabV3Config(), {'N_EPOCHS': 100, 'TRAIN_BATCH_SIZE': 12, 'USE_STEP_DECAY': False,
+    #                                     'AUGMENT_DATA': False, 'LOSS': WEIGHTED_CROSS_ENTROPY_LOSS,
+    #                                     'INCLUDE_BACKGROUND': True}, class_weights=CLASS_WEIGHTS)
+    oai_train.fine_tune(dirpath='/bmrNAS/people/arjun/msk_seg_networks/best_network/deeplabv3_2d/2018-11-27-00-40-24/',
+                        config=DeeplabV3Config(create_dirs=False),
+                        vals_dict={'N_EPOCHS': 100, 'TRAIN_BATCH_SIZE': 12, 'USE_STEP_DECAY': False,
+                                   'AUGMENT_DATA': False, 'LOSS': FOCAL_LOSS,
+                                   'INITIAL_LEARNING_RATE': 8e-6}
+                        )
