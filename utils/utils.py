0ab66673a70b9df3b62627bc902ee9943a74edcf
Arjun Desai <arjunaashna@gmail.com>
2019-02-27 15:12:08 -0800

    fix imports

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+import ast
 import os
 
 from utils.im_utils import save_ims
@@ -29,6 +30,33 @@ def get_weights(base_folder):
             best_file = file_fullpath
 
     return best_file
+
+
+def convert_data_type(var_string, data_type):
+    """
+    Convert string to relevant data type
+    :param var_string: variable as a string (e.g.: '[0]', '1', '2.0', 'hellow')
+    :param data_type: the type of the data
+    :return: string converted to data_type
+    """
+    if (data_type is str):
+        return var_string
+
+    if (data_type is float):
+        return float(var_string)
+
+    if (data_type is int):
+        return int(var_string)
+
+    if (data_type is bool):
+        return ast.literal_eval(var_string)
+
+    if (data_type is list):
+        return ast.literal_eval(var_string)
+
+    if (data_type is tuple):
+        return ast.literal_eval(var_string)
+
 
 
 if __name__ == '__main__':
