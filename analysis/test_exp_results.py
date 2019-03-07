39e9ccb9d05e6329cc854e78919a7afb603de6f4
Arjun Desai <arjunaashna@gmail.com>
2019-03-07 11:07:04 -0800

    upda

--- analysis/test_exp_results.py
+++ analysis/test_exp_results.py
@@ -35,7 +35,7 @@ class TestResults(unittest.TestCase):
             # get best weights in the base folder
             ind = exp_filepath.find('test_results')
             base_folder = exp_filepath[:ind]
-            expected_best_weights = utils.get_weights(base_folder)
+            expected_best_weights = os.path.basename(utils.get_weights(base_folder))
 
             assert weights == expected_best_weights, 'weights %s not expected %s- rerun test for %s: %s' % (weights, expected_best_weights, exp, exp_filepath)
             print('%s valid' % exp)
